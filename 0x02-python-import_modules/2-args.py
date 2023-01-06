6016380437bd40e50c7c96279621134dbfc482ca
angelotheman <ktwum721@gmail.com>
2023-01-06 12:42:08 +0000

    Changes made

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
-message = ""
-if len(sys.argv) == 1:
-    message = "argument"
-else:
-    message = "arguments"
-print("{} {}:".format(len(sys.argv), message))
-[print("{}: {}".format(x, y)) for x , y in enumerate(sys.argv, start=1)]
+if __name__ == '__main__':
+    import sys
+    message = ""
+    if len(sys.argv) == 1:
+        message = "argument"
+    else:
+        message = "arguments"
+    print("{} {}:".format(len(sys.argv), message))
+    [print("{}: {}".format(x, y)) for x , y in enumerate(sys.argv, start=1)]
