8ab516420fbb914d6360f9e32fef730ac85f953b
angelotheman <ktwum721@gmail.com>
2023-01-24 21:54:04 +0000

    Files started

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+class Square:
+    """This is just a raw skeleton of the Square class with no attributes"""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+class Square:
+    """Instantiation wih size for this class"""
+    size = 0
