25243b5baa7e90c80161bf8236fc42540b4487a7
Kwabena Twumasi <ktwum721@gmail.com>
2023-10-31 22:56:13 +0000

    Linked List insert

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+  * insert_node - Inserts a node in a position of the linkedlist
+  * @head: the linked list
+  * @number: Data to insert
+  *
+  * Return: New node or NULL if otherwise
+  */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	unsigned int count = 0;
+	listint_t *curr, *new_node;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = number;
+
+	curr = *head;
+
+	while (curr)
+	{
+		if (number > curr->n && number < curr->next->n)
+		{
+			new_node->next = curr->next;
+			curr->next = new_node;
+			return (new_node);
+		}
+		curr = curr->next;
+	}
+
+	free(new_node);
+	return (NULL);
+}
