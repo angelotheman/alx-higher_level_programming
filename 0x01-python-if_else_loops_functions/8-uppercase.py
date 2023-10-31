2d89908b35f9dc1add1c84c041f9aa2c6fddcc5d
Kwabena Twumasi <ktwum721@gmail.com>
2023-10-31 17:12:57 +0000

    Upper case

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+   for c in str:
+       ascii_value = ord(c)
+       if ord('a') <= ascii_value <= ord('z'):
+           ascii_value -= 32
+       print(chr(ascii_value), end="")
+   print()
