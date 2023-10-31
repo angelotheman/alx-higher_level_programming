0667c3a328bfc17d695ed4a5aaf3990dedf6babb
Kwabena Twumasi <ktwum721@gmail.com>
2023-10-31 17:15:38 +0000

    Upper Case

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-   for c in str:
-       ascii_value = ord(c)
-       if ord('a') <= ascii_value <= ord('z'):
-           ascii_value -= 32
-       print("{}".format(chr(ascii_value)), end="")
-   print()
+    for c in str:
+        ascii_value = ord(c)
+        if ord('a') <= ascii_value <= ord('z'):
+            ascii_value -= 32
+        print("{}".format(chr(ascii_value)), end="")
+    print()
