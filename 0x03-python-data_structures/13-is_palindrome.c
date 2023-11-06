318e202f60934e2038476ed79dfb29f0898cf211
Kwabena Twumasi <ktwum721@gmail.com>
2023-11-06 23:27:35 +0000

    Check if palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+  * is_palindrome - Checks if the linkedlist is palidrome
+  * @head: The linked list to be checked
+  *
+  * Return: 1 if a palindrome, otherwise 0
+  */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *temp;
+	int nodeCount, i, j;
+	int arr[];
+
+	temp = *head;
+	nodeCount = 0;
+
+	while (temp != NULL)
+	{
+		nodeCount++;
+		temp = temp->next;
+	}
+
+	arr[nodeCount];
+
+	temp = *head;
+
+	for (i = 0; i < nodeCount; i++)
+	{
+		arr[i] = temp->n;
+		temp = temp->next;
+	}
+
+	for (i = 0, j = nodeCount - 1; i < j; i++, j--)
+	{
+		if (arr[i] != arr[j])
+			return (0);
+	}
+
+	return (1);
+}
