d8b57d4ac14f3a9e342139928aa645e5658ea85c
Kwabena Twumasi <ktwum721@gmail.com>
2023-11-08 21:58:00 +0000

    Best Score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 def best_score(a_dictionary):
     if a_dictionary:
         max_score = max(a_dictionary.values())
-        best_student = [key for key, value in a_dictionary.items() if value == max_score][0]
-        return best_student
+        student = [k for k, v in a_dictionary.items() if v == max_score][0]
+        return student
     return None
