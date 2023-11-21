181c2b84a425288653490907b8de3c84850e3b8c
angelotheman <ktwum721@gmail.com>
2023-11-21 20:23:03 +0000

    Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -52,7 +52,7 @@ class Square:
         """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
 
         self.__size = value
