815bf59416c38954279c7b3e3196d8ccf3b8ccf1
angelotheman <ktwum721@gmail.com>
2023-12-03 01:41:59 +0000

    Change Representation

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -35,7 +35,7 @@ class Rectangle:
 
         Rectangle.number_of_instances += 1
 
-    def __del__(self, width=0, height=0):
+    def __del__(self):
         """Destructor that prints message when instance is deleted"""
         print("Bye rectangle...")
 
