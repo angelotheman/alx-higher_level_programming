187788abbcbced0667f44eeb08873b3e7fac5207
angelotheman <ktwum721@gmail.com>
2023-12-03 04:31:09 +0000

    Solve the nqueens problem

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+Module for this script
+"""
+import sys
+
+
+def is_safe(board, row, col, N):
+    """
+    Check if it's safe to place a queen at a position
+    (row, col) on the board
+    """
+    for i in range(row):
+        if board[i] == col or \
+           board[i] - i == col - row or \
+           board[i] + i == col + row:
+               return False
+
+    return True
+
+def solve_nqueens(board, row, N):
+    """Recursively solve the N-Queens problem"""
+    if row == N:
+        print([[i, board[i]] for i in range(N)])
+        return
+
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            board[row] = col
+            solve_nqueens(board, row + 1, N)
+
+def nqueens(N):
+    """Main function to solve the N-Queens problem"""
+    if not isinstance(N, int):
+        print("N must be a number")
+        sys.exit(1)
+
+    if N < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [-1] * N
+    solve_nqueens(board, 0, N)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: {} N".format(sys.argv[0]))
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    nqueens(N)
