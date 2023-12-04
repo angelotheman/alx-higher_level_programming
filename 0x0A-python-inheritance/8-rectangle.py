3e410410359769a1ad6bd08c6894d3f941bf9cf9
angelotheman <ktwum721@gmail.com>
2023-12-04 21:42:54 +0000

    Rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module - 8
+init method: width and height
+"""
+
+
+class Rectangle(BaseGeometry):
+    """The Rectangle Class"""
+
+    def __init__(self, width, height):
+        """Initializes the class
+
+        Args:
+        - width: Width of the figure
+        - height: Height of the figure
+        """
+
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+        self.__width = width
+        self.__height = height
