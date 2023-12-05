9e8a3f486a0d91d40d59f9315b56793d19724ee7
angelotheman <ktwum721@gmail.com>
2023-12-05 22:48:42 +0000

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#/usr/bin/python3
 """
 Module - 7
 - Add items and save them in a file
@@ -11,7 +11,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if __name__ == '__main__':
-    item_list = load_from_json_file("add_item.json")
+    try:
+        item_list = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        item_list = []
 
     item_list.extend(sys.argv[1:])
 
