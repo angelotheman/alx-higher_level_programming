d4bb581d04fd44e2df28ca8095909f946a1b1932
angelotheman <ktwum721@gmail.com>
2023-12-05 15:00:36 +0000

    Write to the file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module - 1
+- Implement a write function
+"""
+
+
+def write_file(filename="", text=""):
+    """Writes to a file"""
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
