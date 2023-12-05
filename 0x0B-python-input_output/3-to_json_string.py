01023996e575ac09718dd10407cc99acb1d64f33
angelotheman <ktwum721@gmail.com>
2023-12-05 16:51:13 +0000

    Json to string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module - 3
+- Implement JSON on objects
+"""
+
+
+def to_json_string(my_obj):
+    """Returns JSON representation of object"""
+    data = json.dumps(my_obj)
+    return data
