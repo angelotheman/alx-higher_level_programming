135f94e92a348722f869dcc9eccf08ba2316ee8d
angelotheman <ktwum721@gmail.com>
2023-12-05 23:14:21 +0000

    Class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module - 8
+Class to JSON
+"""
+import json
+
+
+def class_to_json(obj):
+    """Returns the dictionary description"""
+    json_string = json.dumps(data)
+    data = json.loads(json_string)
+    return data
