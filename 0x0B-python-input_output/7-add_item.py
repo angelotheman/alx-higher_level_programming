5d5718fb197a114bb95f5b6d4adcc2cf7bebfbc9
angelotheman <ktwum721@gmail.com>
2023-12-05 22:00:44 +0000

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,8 +6,8 @@ Module - 7
 import sys
 
 
-save_to_json_file = __import__('5-save_to_json').save_to_json_file
-load_from_json_file = __import__('6-load_from_json').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if __name__ == '__main__':
