d4b680ef1687035a2248b1aa1c188f3c2051c18e
angelotheman <ktwum721@gmail.com>
2023-12-05 21:58:35 +0000

    Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module - 7
+- Add items and save them in a file
+"""
+import sys
+
+
+save_to_json_file = __import__('5-save_to_json').save_to_json_file
+load_from_json_file = __import__('6-load_from_json').load_from_json_file
+
+
+if __name__ == '__main__':
+    item_list = load_from_json_file("add_item.json")
+
+    item_list.extend(sys.argv[1:])
+
+    save_to_json_file(item_list, "add_items.json")
