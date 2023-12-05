ad557e4876a24cd8086968566cff7f0e802ca50c
angelotheman <ktwum721@gmail.com>
2023-12-05 17:03:22 +0000

    Json to string object

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module - 4
+- Implement JSON on objects
+"""
+import json
+
+
+def from_json_string(my_str):
+    """Returns JSON representation of object"""
+    data = json.loads(my_str)
+    return data
