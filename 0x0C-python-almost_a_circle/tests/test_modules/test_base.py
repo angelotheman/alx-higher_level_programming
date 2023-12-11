9e38b5e888d5b628f8b2ec7e8e3689f23c0049f9
angelotheman <ktwum721@gmail.com>
2023-12-11 23:56:06 +0000

    Test file for base added

---
+++ 0x0C-python-almost_a_circle/tests/test_modules/test_base.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This is a test case file
+"""
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    """This is the test case for the Base file"""
+
+    def test_base_constructor_with_id(self):
+        """Tests the base constructor"""
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+
+        b3 = Base(12)
+        self.assertEqual(b3.id, 12)
+
+        b4 = Base()
+        self.assertEqual(b4.id, 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
