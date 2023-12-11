cca829d3615d480bf19495f4cae5066490665060
angelotheman <ktwum721@gmail.com>
2023-12-11 20:39:07 +0000

    Updated class methods

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,10 +4,13 @@ Module - Base Class
 
 - Base of all classes
 - private class instance: __nb_objects
-- staticmethod: 
+- staticmethod:
     - def to_json_string(list_dictionaries)
     - def from_json_string(json_string)
-- classmethod: def save_to_file(cls, list_objs)
+- classmethod:
+    - def save_to_file(cls, list_objs)
+    - def create(cls, **dictionary)
+    - def load_from_file(cls)
 """
 
 
@@ -45,9 +48,42 @@ class Base:
         if list_objs is None:
             data = "[]"
         else:
-            data = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+            data = cls.to_json_string([obj.to_dictionary()
+                for obj in list_objs])
 
         filename = cls.__name__ + "json"
 
         with open(filename, "w", encoding="UTF-8") as textfile:
             textfile.write(data)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes set using **dictionary"""
+        if cls.__name__ == "Rectangle":
+            dummy_instance = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy_instance = cls(1)
+        else:
+            raise NotImplementedError("create method not implemented \
+                    for class {}".format(cls.__name__))
+
+        dummy_instance.update(**dictionary)
+        return dummy_instance
+
+    @classmethod
+    def load_from_file(cls):
+        """Loads instance from class"""
+        filename = cls.__name__ + "json"
+
+        try:
+            with open(filename, "r", encoding="UTF-8") as textfile:
+                json_string = textfile.read()
+        except FileNotFoundError:
+            return []
+
+        if not json_string:
+            return []
+
+        json_list = json.loads(json_string)
+        instances = [cls.create(**data) for data in json_list]
+        return instances
