d9177e3b8419ff0411b58f045e068129a0bbdd04
angelotheman <ktwum721@gmail.com>
2023-12-12 01:34:22 +0000

    Updated access modifiers

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -40,10 +40,10 @@ class Rectangle(Base):
             id (int, optional): Identifier of the rectangle
         """
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
