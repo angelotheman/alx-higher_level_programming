3f4f00774e561d79cdfc76292f15a6966a706719
angelotheman <ktwum721@gmail.com>
2023-12-12 01:49:17 +0000

    Updated string method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -140,5 +140,5 @@ class Rectangle(Base):
 
     def __str__(self):
         """Returns human readable object class"""
-        return "[Rectangle] (self.id) self.__x/self.__y - \
-                self.__width/self.__height"
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - \
+                {self.__width}/{self.__height}"
