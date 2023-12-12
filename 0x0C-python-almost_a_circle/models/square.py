3f13559ae2715aa8c7b1f6eb8c42234b29d364a2
angelotheman <ktwum721@gmail.com>
2023-12-12 01:03:33 +0000

    Updated the import statements

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,7 +9,7 @@ Module - Square class
     - Update methods with little tweak
     - Dictionary methods
 """
-Rectangle = __import__("Rectangle").Rectangle
+Rectangle = __import__("models.rectangle").Rectangle
 
 
 class Square(Rectangle):
