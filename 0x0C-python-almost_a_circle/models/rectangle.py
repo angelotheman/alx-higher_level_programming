e5b2e08ef3853dd09b89ba1065d9b9c163b6b2f0
angelotheman <ktwum721@gmail.com>
2023-12-12 01:07:03 +0000

    Hot fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -37,7 +37,7 @@ class Rectangle(Base):
             y (int, optional): y-coordinate of the rectangle's position
             id (int, optional): Identifier of the rectangle
         """
-        super().__init__()
+        super().__init__(id)
         self.__width = width
         self.__height = height
         self.__x = x
