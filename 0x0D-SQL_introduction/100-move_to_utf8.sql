baa22392f534bdfc838b6cfc44fb39670c533966
Kwabena Twumasi <ktwum721@gmail.com>
2024-01-16 19:36:37 +0000

    Move to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,6 +1,5 @@
 -- Convert to utf8mb4 collate utf8mb4_unicode_ci
 
-USE	`hbtn_0c_0`;
-
+USE	`hbtn_0c_0`
 ALTER `first_table`
 MODIFY `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
