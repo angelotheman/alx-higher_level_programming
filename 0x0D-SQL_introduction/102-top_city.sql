581731639724f5d37bfe3a23f0e002c7d1b79fb3
Kwabena Twumasi <ktwum721@gmail.com>
2024-01-16 20:26:00 +0000

    Top city temperature

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,8 +1,8 @@
 -- Display top 3 cities during July and August
 
-SELECT `city`, MAX(value) AS `avg_temp`
+SELECT `city`, AVG(value) AS `avg_temp`
 FROM `temperatures`
-WHERE month IN (7, 8)
+WHERE `month` IN (7, 8)
 GROUP BY `city`
 ORDER BY `avg_temp` DESC;
 LIMIT 3;
