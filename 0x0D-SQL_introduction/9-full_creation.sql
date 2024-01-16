acb03cc0cfb9356f625503969c23f1322897f0f5
Kwabena Twumasi <ktwum721@gmail.com>
2024-01-16 16:20:09 +0000

    full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,13 +2,9 @@
 -- Create multiple rows in that table
 -- Table name = second_table
 
-CREATE TABLE IF NOT EXISTS `second_table` (
-	`id` INT AUTO_INCREMENT,
-	`name` VARCHAR(256),
-	`score` INT
-);
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT AUTO_INCREMENT, `name` VARCHAR(256), `score` INT);
 
-INSERT INTO `second_table` (`name`, `score`) VALUES
+INSERT INTO `second_table` (`name`, `score`) VALUES (
 	("John", 10),
 	("Alex", 3),
 	("Bob", 14),
