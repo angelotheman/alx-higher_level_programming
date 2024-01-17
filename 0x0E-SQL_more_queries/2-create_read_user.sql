570389e212858b0910057e95237bdf7a767b5d4b
Kwabena Twumasi <ktwum721@gmail.com>
2024-01-17 14:29:28 +0000

    Create user and grant select

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -10,7 +10,7 @@ CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 -- Grant select Privilege on database
-GRANT SELECT ON `hbtn_0d_2`.* TO `user_0d_2` WITH GRANT OPTION;
+GRANT SELECT ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost` WITH GRANT OPTION;
 
 -- Flush Privileges
 FLUSH PRIVILEGES;
