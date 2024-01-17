ae5faf63c9882166f1be0fa96fc0dc45024f8828
Kwabena Twumasi <ktwum721@gmail.com>
2024-01-17 14:08:00 +0000

    Create user with privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,11 +1,11 @@
 -- Create a user if not exists
 -- Password should be set
 
-CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY `user_0d_1_pwd`;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 
 -- All privileges on my server
-GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost` WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
 -- FLUSH PRIVILEGES
 FLUSH PRIVILEGES;
