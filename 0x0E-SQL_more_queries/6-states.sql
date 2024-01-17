fdd7a758d41f90541943f9bc32956b135f962066
Kwabena Twumasi <ktwum721@gmail.com>
2024-01-17 15:53:36 +0000

    States

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- Database hbtn_0d_usa
+-- Table states
+
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+
+CREATE TABLE IF NOT EXISTS `states` (
+	`id` INT UNIQUE AUTO_INCEREMENT PRIMARY KEY,
+	`name` VARCHAR(256) NOT NULL
+);
