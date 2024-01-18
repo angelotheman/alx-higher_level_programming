64122d3f08a6646820e7230ca1e8bbb06626e5e3
Kwabena Twumasi <ktwum721@gmail.com>
2024-01-18 03:28:16 +0000

    Cities updated

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,9 +5,9 @@
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
-	`id` INT UNIQUE AUTO_INCREMENT NOT NULL,
-	`name` VARCHAR(256) NOT NULL,
-	`state_id` INT NOT NULL,
-	PRIMARY KEY (id)
-	FOREIGN KEY(`state_id`) REFERENCES `states`(`id`)
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY(state_id) REFERENCES states(id)
 );
