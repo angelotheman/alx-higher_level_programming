50e9ffe2554b504aa8a2563fc512af41e7d85f93
Kwabena Twumasi <ktwum721@gmail.com>
2024-01-18 05:07:45 +0000

    Rating genres

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -4,9 +4,7 @@ SELECT tg.name, SUM(tsr.rate) AS `rating`
 	FROM tv_genres tg
 		JOIN tv_show_genres tsg
 			ON tg.id = tsg.genre_id
-		JOIN tv_shows ts
-			ON ts.id = tsg.show_id
 		JOIN tv_show_ratings tsr
-			ON ts.id = tsr.show_id
+			ON tsg.show_id = tsr.show_id
 	GROUP BY `tg.name`
 	ORDER BY `rating` DESC;
