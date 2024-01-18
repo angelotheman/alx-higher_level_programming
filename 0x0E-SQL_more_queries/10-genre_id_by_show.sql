586fe03c69f171d59541f2c1611046d2a9039c74
Kwabena Twumasi <ktwum721@gmail.com>
2024-01-18 00:44:26 +0000

    Genre id by show

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 -- List all shows in the database
 
 SELECT ts.title, tsg.genre_id
-	FROM tv_shows ts, tv_shows_genres tsg
+	FROM tv_shows ts, tv_show_genres tsg
 WHERE tsg.show_id = ts.id
-ORDER BY ts.title ASC, tsg.genre_id ASC;
+ORDER BY ts.title, tsg.genre_id;
