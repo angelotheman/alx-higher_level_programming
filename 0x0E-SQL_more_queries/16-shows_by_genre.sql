b6658af2041b32349691db83c94a0776e6a96235
Kwabena Twumasi <ktwum721@gmail.com>
2024-01-18 02:12:31 +0000

    Shows by genre

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,7 +2,7 @@
 
 SELECT ts.title, tg.name
 	FROM tv_shows ts
-		LEFT JOIN tv_show_genre tsg
+		LEFT JOIN tv_show_genres tsg
 		ON ts.id = tsg.show_id
 		LEFT JOIN tv_genres tg
 		ON tg.id = tsg.genre_id
