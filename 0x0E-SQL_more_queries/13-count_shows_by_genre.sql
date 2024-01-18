82c1b8f7d7c3795d0e203531d6a1a3b078ae9c4b
Kwabena Twumasi <ktwum721@gmail.com>
2024-01-18 01:22:30 +0000

    Count shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,6 +5,6 @@ SELECT
 	Count(tsg.genre_id) AS `number_of_shows`
 	FROM tv_genres tg
 		JOIN tv_show_genres tsg
-		ON tg.id = tsh.genre_id
+		ON tg.id = tsg.genre_id
 	GROUP BY `genre`
 	ORDER BY `number_of_shows` DESC;
