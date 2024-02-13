f3ba18b5219c223c0cfcb41bfb9f58cc018f4f68
Kwabena Twumasi <ktwum721@gmail.com>
2024-02-13 23:16:37 +0000

    Occurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const nbOccurences = (list, searchElement) => {
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+  }
+
+  return count;
+};
+
+exports.nbOccurences = nbOccurences;
