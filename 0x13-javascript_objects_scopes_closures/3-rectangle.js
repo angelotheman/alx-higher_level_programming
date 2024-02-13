e702172deeb0ff92b5d9c5e7cfe604815fbae34d
Kwabena Twumasi <ktwum721@gmail.com>
2024-02-13 17:22:16 +0000

    Rectangle #3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const Rectangle = class {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    while (this.height > 0) {
+      console.log('X'.repeat(this.width));
+      h--;
+    }
+};
+
+module.exports = Rectangle;
