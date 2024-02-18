809e7bd76c9cb8b25545bec0c5bbd2646add311a
Kwabena Twumasi <ktwum721@gmail.com>
2024-02-18 21:52:59 +0000

    Filer cities safe

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module to list all states
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+
+    cursor = db.cursor()
+
+    query = """
+    SELECT cities.name
+    FROM cities
+    JOIN states
+        ON cities.state_id = states.id
+    WHERE states.name = %s
+    ORDER BY cities.id
+    """
+
+    cursor.execute(query, (argv[4],))
+
+    result = cursor.fetchall()
+
+    for state in result:
+        print(state)
