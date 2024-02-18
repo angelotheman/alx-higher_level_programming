83b1240b90d84899f93f13614413c991ec6c383e
Kwabena Twumasi <ktwum721@gmail.com>
2024-02-18 21:13:31 +0000

    SQL Injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module to list all states
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+
+    cursor = db.cursor()
+
+    query = "SELECT * FROM states WHERE BINARY name = %s"
+
+    cursor.execute(query, (argv[4]))
+
+    result = cursor.fetchall()
+
+    for state in result:
+        print(state)
