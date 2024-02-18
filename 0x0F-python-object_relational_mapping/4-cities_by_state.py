f1331f168b2d125ca1057a1cda21792e64534911
Kwabena Twumasi <ktwum721@gmail.com>
2024-02-18 21:25:59 +0000

    Cities by State

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,13 @@ if __name__ == '__main__':
 
     cursor = db.cursor()
 
-    query = "SELECT * FROM cities ORDER BY id"
+    query = """
+    SELECT cities.id, cities.name, states.name
+    FROM cities
+    JOIN states
+        ON cities.state_id = states.id
+    ORDER BY cities.id
+    """
 
     cursor.execute(query)
 
