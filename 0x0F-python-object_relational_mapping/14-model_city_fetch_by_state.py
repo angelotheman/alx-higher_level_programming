9bed1d6f69b59d908cc9701c214fde9f8356be31
Kwabena Twumasi <ktwum721@gmail.com>
2024-02-19 11:52:16 +0000

    Model city fetch by state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -18,5 +18,5 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for city, state in session.query(City).join(State):
+    for city, state in session.query(City, State).filter(City.state_id == State.id):
         print("{}: ({}) {}".format(state.name, city.id, city.name))
