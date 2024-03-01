9248cd10172b0e827c411f5f9d8d0ba06bcb0b72
Kwabena Twumasi <ktwum721@gmail.com>
2024-03-01 12:43:12 +0000

    Post parameter

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# DELETE method using curl
+# POST method using curl
 curl -sX POST -d '{"email": "test@gmail.com", "subject": "I will always be here for PLD"}' -H "Content-Type: application/json" "$1"
