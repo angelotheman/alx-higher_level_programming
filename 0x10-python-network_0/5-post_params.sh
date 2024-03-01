abaa52c808dc1fe8e5a21e418d361ddea37e2162
Kwabena Twumasi <ktwum721@gmail.com>
2024-03-01 13:06:53 +0000

    Post parameter

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # POST method using curl
-curl -sX POST -d '{"email": "test@gmail.com", "subject": "I will always be here for PLD"}' -H "Content-Type: application/json" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
