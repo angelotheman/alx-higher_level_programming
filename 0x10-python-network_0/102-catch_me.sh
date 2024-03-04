97cf97143cfc660f9913ca5e719237594a3df6b8
Kwabena Twumasi <ktwum721@gmail.com>
2024-03-04 11:36:13 +0000

    Catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # POST method using curl
-curl -sX POST -H "User-Agent: You got me!" http://0.0.0.0:5000/catch_me
+curl -s -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
