83ea53af90b13099c69bac7a352f2699dd0f51e1
Kwabena Twumasi <ktwum721@gmail.com>
2024-03-26 17:34:24 +0000

    Starwars semistandard fixed

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = process.argv[2]
+const id = process.argv[2];
 const URL = 'https://swapi-api.alx-tools.com/api/films/' + id;
 
 request(URL, (error, response, body) => {
