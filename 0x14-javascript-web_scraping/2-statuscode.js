19fd1a308ad9bb3f405bd0748238b961862e1c6e
Kwabena Twumasi <ktwum721@gmail.com>
2024-03-26 17:20:34 +0000

    Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -7,6 +7,6 @@ request(pathURL, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
-    console.log(response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
