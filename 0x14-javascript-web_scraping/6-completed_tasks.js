eab30219370cc6d0407e60841b044b51f32bfc7b
Kwabena Twumasi <ktwum721@gmail.com>
2024-03-27 11:35:24 +0000

    Completed task with dictionary

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const apiUrl =  process.argv[2];
+const apiUrl = process.argv[2];
 
 request(apiUrl, (error, response, body) => {
   if (error) {
@@ -14,11 +14,11 @@ request(apiUrl, (error, response, body) => {
 
   todos.forEach(todo => {
     if (todo.completed) {
-        if (completedTasks[todo.userId]) {
-            completedTasks[todo.userId]++;
-        } else {
-            completedTasks[todo.userId] = 1;
-        }
+      if (completedTasks[todo.userId]) {
+        completedTasks[todo.userId]++;
+      } else {
+        completedTasks[todo.userId] = 1;
+      }
     }
   });
   console.log(completedTasks);
