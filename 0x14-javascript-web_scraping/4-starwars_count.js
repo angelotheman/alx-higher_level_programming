34cb984b32339d3b63c77e685def4efd006f463d
Kwabena Twumasi <ktwum721@gmail.com>
2024-03-27 04:18:31 +0000

    Star wars count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+const apiUrl =  process.argv[2];
+const characterId = '18';
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  let count = 0;
+
+  for (let film of data.results) {
+    if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+        count++;
+    }
+  }
+
+  console.log(count);
+});
