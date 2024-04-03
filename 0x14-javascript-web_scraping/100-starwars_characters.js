43ff2d5c44c31f46d02d18b6852c2b679f65f2de
Kwabena Twumasi <ktwum721@gmail.com>
2024-04-03 22:17:35 +0000

    Hot fix

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+
+const request = require('request');
+const movie_id = process.argv[2]
+const apiUrl = 'https://swapi-api.alx-tools.com/api/films/' + movie_id;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const filmData = JSON.parse(body);
+
+	const characterUrls = filmData.characters;
+
+	characterUrls.forEach(characterUrl => {
+		request(characterUrl, (error, response, body) => {
+			if (error) {
+				console.error(error);
+				return;
+			}
+
+			const characterData = JSON.parse(body);
+
+			console.log(characterData.name);
+		});
+	});
+});
