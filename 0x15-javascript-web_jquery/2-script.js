9da7733985ae11160d5956bdd982f6b8ccda7f1c
Kwabena Twumasi <ktwum721@gmail.com>
2024-04-09 12:06:21 +0000

    Fixed for semistandard

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,5 +1,5 @@
-$(function(){
-	$("#red_header").click(function(){
-		$("header").css("color", "#FF0000");
-	});
+$(function () {
+  $('#red_header').click(function () {
+    $('header').css('color', '#FF0000');
+  });
 });
--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-	$("#red_header").click(function(){
-		$("header").addClass("red");
-	});
+$(function () {
+  $('#red_header').click(function () {
+    $('header').addClass('red');
+  });
 });
